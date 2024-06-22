9d5c6efded08660697d10d28a85e1e9393a34a9a
Aayush Thapa <aayushthapa@Aayushs-MacBook-Air.local>
2024-06-22 15:25:51 +0545

    back to normal

--- app.js
+++ app.js
@@ -54,20 +54,6 @@ app.get('/', (req, res) => {
 });
 
 // POST route for form submission
-// app.post('/submit', upload.single('image'), (req, res) => {
-//     const { first_name, last_name, email, hire_date, job_title, salary } = req.body;
-//     const imageUrl = req.file.location; // URL of the uploaded image in S3
-
-//     const sql = 'INSERT INTO employee (first_name, last_name, email, hire_date, job_title, salary, image_url) VALUES (?, ?, ?, ?, ?, ?, ?)';
-//     db.query(sql, [first_name, last_name, email, hire_date, job_title, salary, imageUrl], (err, result) => {
-//         if (err) {
-//             throw err;
-//         }
-//         console.log('Employee data inserted');
-//         res.redirect('/');
-//     });
-// });
-
 app.post('/submit', upload.single('image'), (req, res) => {
     const { first_name, last_name, email, hire_date, job_title, salary } = req.body;
     const imageUrl = req.file.location; // URL of the uploaded image in S3
@@ -75,14 +61,12 @@ app.post('/submit', upload.single('image'), (req, res) => {
     const sql = 'INSERT INTO employee (first_name, last_name, email, hire_date, job_title, salary, image_url) VALUES (?, ?, ?, ?, ?, ?, ?)';
     db.query(sql, [first_name, last_name, email, hire_date, job_title, salary, imageUrl], (err, result) => {
         if (err) {
-            console.error('Error inserting employee data:', err);
-            return res.status(500).json({ success: false, message: 'Database error' });
+            throw err;
         }
         console.log('Employee data inserted');
-        res.json({ success: true, message: 'Employee data inserted successfully' });
+        res.redirect('/');
     });
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
