406bba66328d7c23ce63130a2ca7d9e92fc42a6a
Aayush Thapa <aayushthapa@Aayushs-MacBook-Air.local>
2024-06-22 14:58:58 +0545

    changed port to 3000

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/submit', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
